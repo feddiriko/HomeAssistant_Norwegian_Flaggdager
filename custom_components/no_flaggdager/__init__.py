0428ddc583b23f776439dee297f8488de8fed03d
feddiriko <github@feddiriko.no>
2025-09-04 21:18:36 +0200

    Update __init__.py

--- custom_components/no_flaggdager/__init__.py
+++ custom_components/no_flaggdager/__init__.py
@@ -3,17 +3,18 @@ from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN, PLATFORMS
 from .coordinator import FlaggdagCoordinator
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = FlaggdagCoordinator(hass)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)
-        )
+    # Ny måte å videresende setup til plattformer
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
+
 
 async def async_setup(hass, config):
     return True
