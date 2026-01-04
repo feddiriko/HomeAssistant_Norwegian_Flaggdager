d344a793bd21bca0b1282374f3a060a4e698ef12
feddiriko <github@feddiriko.no>
2026-01-04 19:46:28 +0100

    Add icon attribute to FlaggdagNavnSensor
    Added an icon attribute to FlaggdagNavnSensor.

--- custom_components/no_flaggdager/sensor.py
+++ custom_components/no_flaggdager/sensor.py
@@ -11,7 +11,9 @@ class FlaggdagNavnSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._attr_name = "Flaggdag Navn"
         self._attr_unique_id = "no_flaggdager_flaggdag_navn"
+        self._attr_icon = "mdi:flag-variant" 
 
     @property
     def native_value(self):
         return self.coordinator.data["flaggdag_navn"]
+
